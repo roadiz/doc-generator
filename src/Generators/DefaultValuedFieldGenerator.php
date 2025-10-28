bcc41616b40d920cde86337b56d77117f8139364
roadiz-ci <ambroise+ci@roadiz.io>
2025-10-28 09:05:23 +0000

    chore: Bumped

--- src/Generators/DefaultValuedFieldGenerator.php
+++ src/Generators/DefaultValuedFieldGenerator.php
@@ -6,7 +6,6 @@ namespace RZ\Roadiz\Documentation\Generators;
 
 final class DefaultValuedFieldGenerator extends AbstractFieldGenerator
 {
-    #[\Override]
     public function getContents(): string
     {
         return implode("\n\n", [
@@ -17,8 +16,10 @@ final class DefaultValuedFieldGenerator extends AbstractFieldGenerator
 
     private function getDefaultValues(): string
     {
-        return implode("\n", array_map(fn (string $value) => implode("\n", [
-            '* **'.trim($this->translator->trans(trim($value))).'** `'.$value.'`',
-        ]), $this->field->getDefaultValuesAsArray()))."\n";
+        return implode("\n", array_map(function (string $value) {
+            return implode("\n", [
+                '* **'.trim($this->translator->trans(trim($value))).'** `'.$value.'`',
+            ]);
+        }, $this->field->getDefaultValuesAsArray()))."\n";
     }
 }
