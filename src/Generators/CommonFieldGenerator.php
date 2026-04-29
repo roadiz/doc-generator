a9a17fb757a7ccf049d9da2217e0a94bbaa5367e
roadiz-ci <ambroise+ci@roadiz.io>
2026-04-29 08:08:02 +0000

    Merge branch hotfix/v2.7.19

--- src/Generators/CommonFieldGenerator.php
+++ src/Generators/CommonFieldGenerator.php
@@ -6,10 +6,11 @@ namespace RZ\Roadiz\Documentation\Generators;
 
 final class CommonFieldGenerator extends AbstractFieldGenerator
 {
+    #[\Override]
     public function getContents(): string
     {
         return implode("\n\n", [
-            $this->getIntroduction()
+            $this->getIntroduction(),
         ]);
     }
 }
